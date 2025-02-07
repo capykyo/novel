93ed4206b4b76c96b48581b509ab1cf7339bf870
kyoapps-kyo <kyo@kyoapps.com>
2025-02-07 22:20:34 +0800

    修改页首页脚

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-800 text-white text-center p-4">
-      <p>&copy; 2025 我的公司. 保留所有权利.</p>
+      <p>&copy; 2025 Wuyang.</p>
     </footer>
   );
 };
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,7 @@
 const Header: React.FC = () => {
   return (
     <header className="p-4 text-center">
-      <h1 className="text-2xl">hello</h1>
+      <h1 className="text-2xl">WuYang小说站</h1>
     </header>
   );
 };
