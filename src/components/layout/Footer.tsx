eb88018c41c5e05e1ef90f4c7877736c23bed7a6
ubdmf <sweeety.xinyi@gmail.com>
2025-02-25 15:18:03 +0800

    update: footer样式

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-gray-800 text-white text-center p-4">
-      <p>&copy; 2025 Wuyang.</p>
+    <footer className="bg-gray-100 dark:bg-neutral-800 text-center p-6 shadow-lg mt-10">
+      <p className="text-lg">&copy; 2025 Capykyo.</p>
     </footer>
   );
 };
