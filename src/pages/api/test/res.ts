f009911ab20e552c1f3dc50fd4668f3063bea4fb
ubdmf <sweeety.xinyi@gmail.com>
2025-03-03 04:03:56 +0800

    create: bookinfo api test

---
+++ src/pages/api/test/res.ts
@@ -0,0 +1,149 @@
+const resHtml = `<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1" />
+<title>重生变女王:拿下特种队长 - 全本小说网</title>
+<meta name="keywords" content="重生变女王:拿下特种队长,璎、娜娜,重生变女王:拿下特种队长最新章节" />
+<meta name="description" content="全本小说网 《重生变女王:拿下特种队长》,作者:璎、娜娜,简介:陆念雯不是一个平凡的人,却有一个平凡的人生.渣男误了她一生,痴男等了她一世,另一个本该是打酱油的角色,却偏跑..." />
+<meta name="applicable-device" content="pc,mobile" />
+<link rel="amphtml" href="https://www.quanben.io/amp/n/chongshengbiannuwangnaxiatezhongduizhang/list.html" />
+<style type="text/css">
+body{margin:0 auto;font:1em/150% Arial, Helvetica, sans-serif;background-color:#ededed;text-align:center;max-width:1200px;}
+h1{margin:0px;padding:10px;font-size:1.3em;background-color:#FFF;}
+a{color:#666;text-decoration:none;margin:0px;}a:hover{color:#F00;}
+.l{float:left;}.r{float:right;}.c{clear:both;}
+.tool{ margin: 10px 10px 0px 10px;}.tc{text-align:center;}
+.breadcrumb{padding:10px 20px;background-color:#e7e7e7; text-align: left; font-size: 0.85em; color:#ccc;}
+.breadcrumb a{ padding: 0px;}
+.footer{position:relative;margin:15px 0px 0px 0px;background:#f8f8f8;color:#999;text-align:center;border:1px solid #dddddd;font-size: 0.8em;}
+.footer:before{content:'';position:absolute;left:50%;margin-left:-20px;top:-12px;background:#f8f8f8;width:40px;height:40px;border-radius:20px;z-index:1;border-top:1px solid #dddddd;}
+.footer .copyright{position:relative;z-index:2;line-height:120%;}
+.box{box-shadow:1px 1px 3px #dddddd;background-color:#FFF;text-align:left;margin:2px;}
+.title{clear:both;margin:0px;padding:0 12px;font-weight:normal;font-size:1em;line-height:40px;border:1px solid #FFF;border-bottom:1px solid #e3dddc;background-color:#f7f7f7;}
+.title span{margin:0px;padding:0px 0px 0px 5px;border-left:5px solid #F00;}
+.list3{margin:0px;padding:0px;}
+.list3 li{text-align:left;position:relative;margin:0px 0px;list-style:none;padding:0px;font-size:0.85em;white-space:nowrap;overflow:hidden;}
+.list3 li a{margin:0px;display:block;padding:8px 20px;border-bottom:1px solid #eeeeee;color:#262626;}
+.list3 li a:hover{color:#F00;}
+.list3 li .author{position:absolute;top:0px;right:0px;padding:12px 10px 12px 0px;color:#999;font-size:0.85em;}
+.list2 { background-color:#FFF;text-align:left;margin:1px; display:block; padding:10px 0px 5px 10px; height:90px; border-bottom:1px solid #eeeeee; overflow:hidden; position:relative; }
+.list2 img{ width: 60px; height: 80px;  display:block; float:left; background-color:#FFF; padding:2px; border:1px solid #CCC;}
+.list2 h3{ margin:0px; padding:0px; margin-left:70px; height:28px; line-height:28px; font-size: 18px; font-weight: 400; text-shadow:none; color:#333; }
+.list2 p{margin:0px; padding:0px 2px; margin-left:70px; color:#999; font-size:0.82em; line-height:20px; }
+.list2 p span { color:#666; margin-right:2px; }
+.description{ padding: 10px; color: #999; font-size: 0.85em; }
+.description p{ padding: 0px; margin: 0px; word-break: break-all;white-space: pre-wrap; overflow:hidden;}
+a.button { padding:3px 12px 4px 12px; margin:2px 4px 0px 0px; border:1px solid #ccc; background-color:#FFF; font-size:14px; text-align:center; line-height:16px; color:#666; text-decoration: none; }
+a.button:hover{color:#F00;}
+a.s1{background-color:#F00;border:1px solid #C00; color:#FFF;}
+a.s1:hover { text-decoration: underline; color:#FFF; }
+.tool_button{ padding:10px 0px 0px 20px;}
+.content_more{position:relative;}.red{color:red;}.tc{text-align: center;}
+.more{background-image: linear-gradient(to top,rgba(255,255,255,1) 0%,rgba(255,255,255,1) 30%,rgba(255,255,255,0.5) 50%,rgba(255,255,255,0) 100%);padding: 4em 0 0.2em 0;position: absolute;color:#000;bottom: 0;width:99%;text-align:center;}
+.more a{color: red;}
+.list3 li{width:25%; float: left;}
+@media screen and (min-width:1024px){.list3 li{width:25%;}}
+@media screen and (min-width:800px) and (max-width:1024px){.list3 li{width:33.33%;}}
+@media screen and (min-width:600px) and (max-width:800px){.list3 li{width:50%;}}
+@media screen and (max-width:600px){.list3 li{width:100%;}}
+</style>
+<script type="text/javascript">
+function base64(_str){var staticchars="PXhw7UT1B0a9kQDKZsjIASmOezxYG4CHo5Jyfg2b8FLpEvRr3WtVnlqMidu6cN";var encodechars="";for(var i=0;i<_str.length;i++){var num0=staticchars.indexOf(_str[i]);if(num0==-1){var code=_str[i]}else{var code=staticchars[(num0+3)%62]}var num1=parseInt(Math.random()*62,10);var num2=parseInt(Math.random()*62,10);encodechars+=staticchars[num1]+code+staticchars[num2]}return encodechars}function changefs(size){if(size=='+'){var u=document.getElementById('content').style.fontSize;var a=u.match(/\d/g).join("");var fpx=parseInt(a)+1;document.getElementById('content').style.fontSize=fpx+"px"}else if(size=='-'){var u=document.getElementById('content').style.fontSize;var a=u.match(/\d/g).join("");var fpx=parseInt(a)-1;document.getElementById('content').style.fontSize=fpx+"px"}else{document.getElementById('content').style.fontSize=size+"px"}}function load_more(book){document.getElementById('detail').innerHTML='<div class="red tc">[ loading... ]</div>';var theHead=document.getElementsByTagName('head').item(0);var myScript=document.createElement('script');myScript.src='/index.php?c=book&a=list.jsonp&callback='+callback+'&book_id='+book+'&b='+base64(callback);myScript.type='text/javascript';myScript.defer=true;theHead.appendChild(myScript)};
+var callback='b8d019';
+function db8d019(data) {
+  document.getElementById('detail').innerHTML=data.content;
+}
+</script>
+</head>
+<body itemscope itemtype="http://schema.org/WebPage">
+<h1 itemprop="name headline">重生变女王:拿下特种队长</h1>
+<div class="breadcrumb" itemprop="breadcrumb">当前位置：<a href="/">首页</a> &rsaquo;
+<a href="/c/qingchun.html">青春小说</a> &rsaquo;
+<a href="/n/chongshengbiannuwangnaxiatezhongduizhang/">《重生变女王:拿下特种队长》</a>
+</div>
+<div class="box" itemscope="itemscope" itemtype="http://schema.org/Book">
+  <h2 class="title"><span>书籍简介</span></h2>
+  <div class="list2">
+  	<img src="https://img.c0m.io/quanben.io/upload/thumbnail/book_0_999/book_958.jpg" alt="重生变女王:拿下特种队长" itemprop="image" />
+  	<h3><span itemprop="name">重生变女王:拿下特种队长</span></h3>
+  	<p>作者: <span itemprop="author">璎、娜娜</span></p>
+  	<p>类别: <span itemprop="category">青春</span></p>
+  	<p>状态: <span>完结</span></p>
+  </div>
+  <div class="description" itemprop="description">
+  	<p>陆念雯不是一个平凡的人,却有一个平凡的人生.渣男误了她一生,痴男等了她一世,另一个本该是打酱油的角色,却偏跑到她的世界来充当配角的女人.前世走得崎岖又憋屈,今生这条康庄大道一定要幸福再幸福.&quot;别人说我脾气不好.&quot;某女撒娇中.&quot;我宠的我乐意.&quot;特种队长淡定中.</p>
+  </div>
+</div>
+
+<div class="box">
+	<h2 class="title"><span>章节目录</span></h2>
+    <ul class="list3" itemscope="itemscope" itemtype="http://schema.org/ItemList"><li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/1.html" itemprop="url"><span itemprop="name">chapter1小三这出戏</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/2.html" itemprop="url"><span itemprop="name">chapter2死亡这回事</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/3.html" itemprop="url"><span itemprop="name">chapter3重生这点事</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/4.html" itemprop="url"><span itemprop="name">chapter4一群损友</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/5.html" itemprop="url"><span itemprop="name">chapter5是债还是爱</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/6.html" itemprop="url"><span itemprop="name">chapter6喝醉必须的</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/7.html" itemprop="url"><span itemprop="name">chapter7都是王八蛋</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/8.html" itemprop="url"><span itemprop="name">chapter8被带坏了</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/9.html" itemprop="url"><span itemprop="name">chapter9所谓国粹</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/10.html" itemprop="url"><span itemprop="name">chapter10情场得意</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/11.html" itemprop="url"><span itemprop="name">chapter11小孩子脾气</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/12.html" itemprop="url"><span itemprop="name">chapter12那个男人</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/13.html" itemprop="url"><span itemprop="name">chapter13无理取闹</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/14.html" itemprop="url"><span itemprop="name">chapter14女王占有欲</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/15.html" itemprop="url"><span itemprop="name">chapter15他的宠溺</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/16.html" itemprop="url"><span itemprop="name">chapter16柳下惠</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/17.html" itemprop="url"><span itemprop="name">chapter17麻烦制造机</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/18.html" itemprop="url"><span itemprop="name">chapter18不知道的事</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/19.html" itemprop="url"><span itemprop="name">chapter19所谓朋友</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/20.html" itemprop="url"><span itemprop="name">chapter20不再联系</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/21.html" itemprop="url"><span itemprop="name">chapter21工于心计</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/22.html" itemprop="url"><span itemprop="name">chapter22初见</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/23.html" itemprop="url"><span itemprop="name">chapter23模糊的曾经</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/24.html" itemprop="url"><span itemprop="name">chapter23有机会补救</span></a></li>
+</ul><div class="c"></div><div class="content_more" id="detail"><div class="more">... <a href="javascript:void(0)" onclick="load_more('958')">[展开完整列表]</a> ...</div></div><ul class="list3" itemscope="itemscope" itemtype="http://schema.org/ItemList"><li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/314.html" itemprop="url"><span itemprop="name">番外纵情的后果</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/315.html" itemprop="url"><span itemprop="name">番外尴尬的话题</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/316.html" itemprop="url"><span itemprop="name">番外不舒服</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/317.html" itemprop="url"><span itemprop="name">番外安抚</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/318.html" itemprop="url"><span itemprop="name">番外去海边吧</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/319.html" itemprop="url"><span itemprop="name">番外马尔代夫之旅①</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/320.html" itemprop="url"><span itemprop="name">番外马代之旅②</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/321.html" itemprop="url"><span itemprop="name">番外马代之旅③</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/322.html" itemprop="url"><span itemprop="name">番外马代之旅④</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/323.html" itemprop="url"><span itemprop="name">番外马代之旅⑤</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/324.html" itemprop="url"><span itemprop="name">番外马代之旅⑥</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/325.html" itemprop="url"><span itemprop="name">番外马代之旅7</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/326.html" itemprop="url"><span itemprop="name">番外马代之旅⑧</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/327.html" itemprop="url"><span itemprop="name">番外马代之旅⑨</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/328.html" itemprop="url"><span itemprop="name">番外马代之旅10</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/329.html" itemprop="url"><span itemprop="name">番外马代之旅11</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/330.html" itemprop="url"><span itemprop="name">番外马代之旅12</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/331.html" itemprop="url"><span itemprop="name">番外马代之旅13</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/332.html" itemprop="url"><span itemprop="name">番外马代之旅14</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/333.html" itemprop="url"><span itemprop="name">番外马代之旅15</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/334.html" itemprop="url"><span itemprop="name">番外马代之旅16</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/335.html" itemprop="url"><span itemprop="name">番外马代之旅17</span></a></li>
+<li itemprop="itemListElement"><a href="/n/chongshengbiannuwangnaxiatezhongduizhang/336.html" itemprop="url"><span itemprop="name">番外马代之旅18</span></a></li>
+</ul><div class="c"></div>
+</div>
+<div class="footer">
+<p class="copyright">quanben.io</p>
+</div>
+</body>
+</html>`;
+
+const expectRes = {
+  title: "重生变女王:拿下特种队长",
+  img: "https://img.c0m.io/quanben.io/upload/thumbnail/book_0_999/book_958.jpg",
+  description:
+    '陆念雯不是一个平凡的人,却有一个平凡的人生.渣男误了她一生,痴男等了她一世,另一个本该是打酱油的角色,却偏跑到她的世界来充当配角的女人.前世走得崎岖又憋屈,今生这条康庄大道一定要幸福再幸福."别人说我脾气不好."某女撒娇中."我宠的我乐意."特种队长淡定中.',
+  lastChapterNumber: "336",
+  url: "https://quanben.io/n/chongshengbiannuwangnaxiatezhongduizhang/",
+  currentChapter: "1",
+};
+
+export const bookInfo = {
+  resHtml,
+  expectRes,
+};
