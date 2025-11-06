2cdbe877c4b9c529ef437ed443018b331924965c
ubdmf <sweeety.xinyi@gmail.com>
2025-11-06 22:38:20 +0800

    refactor: enhance localStorage handling and improve error management across components

--- src/utils/localStorageHelper.ts
+++ src/utils/localStorageHelper.ts
@@ -1,4 +1,5 @@
 import { storage } from "@/utils/storage";
+<<<<<<< HEAD
 import { BookProps } from "@/types/book";
 
 // 更新BookInfo第一本书的currentChapter
@@ -6,6 +7,14 @@ export const updateBookCurrentChapter = (currentChapter: number) => {
   const bookInfoObj = storage.get<BookProps[]>("bookInfo", []);
   if (bookInfoObj && bookInfoObj.length > 0) {
     bookInfoObj[0].currentChapter = currentChapter.toString();
+=======
+
+// 更新BookInfo第一本书的currentChapter
+export const updateBookCurrentChapter = (currentChapter: number) => {
+  const bookInfoObj = storage.get<any[]>("bookInfo", []);
+  if (bookInfoObj && bookInfoObj.length > 0) {
+    bookInfoObj[0].currentChapter = currentChapter;
+>>>>>>> 186f60b (refactor: enhance localStorage handling and improve error management across components)
     storage.set("bookInfo", bookInfoObj);
   }
 };
